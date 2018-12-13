64b00151b671661dbb42bb6ac2a1e86532be75c0
Wayne Collier <ke4tri@gmail.com>
2018-12-12 19:33:48 -0600

    firebase

---
+++ src/components/Auth/auth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
+import './auth.scss';
+
+
+class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+    }).catch(err => console.error('there was an error', err));
+  }
+
+  render() {
+    return (
+      <div className="Auth">
+        <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
